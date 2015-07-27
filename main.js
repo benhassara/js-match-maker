9e9d22f29a36cc504a09d1c2394c66c96944ffba
benhassara <bhassara@gmail.com>
2015-07-27 12:14:57 -0600

    squashed annoying show mentors bug

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ function createPerson(first, last, phone, city) {
 }
 
 function grabValues() {
+  //grab the form data
   var first = document.getElementById('firstName').value;
   var last = document.getElementById('lastName').value;
   var phone = document.getElementById('phone').value;
@@ -26,7 +27,7 @@ function grabValues() {
 function verifyPhone(phn) {
   if (phn.length !== 12)
     return false;
-  else if (!(/\d{3}-\d{3}-\d{4}/.test(phn)))
+  else if (!(/\d{3}-\d{3}-\d{4}$/.test(phn)))
     return false;
   else {
     return true;
@@ -57,34 +58,93 @@ function addAnother() {
   }
 }
 
+function subAll() {
+  chooseCity();
+  var citySelect = document.getElementsByTagName('select')[1];
+  console.log(citySelect);
+  citySelect.addEventListener('change', function(){showMentors();});
+}
+
 function chooseCity(e) {
-  // e.preventDefault();
   var cities = [];
 
   for (var i = 0; i < mentors.length; i++) {
-    console.log(mentors[i]);
     cities.push(mentors[i].city);
   }
 
   var newRow = document.createElement('div');
   var choose = document.createElement('div');
   var newBtn = document.createElement('button');
+
   newRow.className = 'row padded-top';
   choose.className = 'col-xs-12';
   newBtn.className = 'btn btn-primary';
   newBtn.innerHTML = 'Go';
+  newBtn.id = 'go';
 
   choose.appendChild(addSelect(cities));
   choose.appendChild(newBtn);
   newRow.appendChild(choose);
   document.getElementsByClassName('container')[0].appendChild(newRow);
+}
+
+function getMentors(city) {
+  //return array of mentors in city
+  var mentorsInCity = [];
+  for (var i = 0; i < mentors.length; i++) {
+    if (mentors[i].city === city)
+      mentorsInCity.push(mentors[i]);
+  }
+  return mentorsInCity;
+}
+
+function showMentors() {
+  var mentRow = document.createElement('div');
+  var container = document.getElementsByClassName('container')[0];
+  var mentorsInCity = getMentors(document.getElementsByTagName('select')[1].value);
+
+  mentRow.className = "row padded-top";
+
+  for (var i = 0; i < mentorsInCity.length; i++) {
+    console.log('formatOut: ', formatOutput(mentorsInCity[i]));
+    mentRow.appendChild(formatOutput(mentorsInCity[i]));
+  }
+
+  container.appendChild(mentRow);
+}
+
+function formatOutput(mentor) {
+  var newColumn = document.createElement('div');
+  var newList = document.createElement('ul');
+  newColumn.className = "col-xs-12 col-md-6";
+
+  createAndAppend(newList, 'li', "Name: " + mentor.full);
+  createAndAppend(newList, 'li', "Phone: " + mentor.phone);
+  createAndAppend(newList, 'li', "City: " + mentor.city);
+
+  newColumn.appendChild(newList);
+
+  return newColumn;
 
 }
 
+function createAndAppend(parent, tag, text) {
+  var newElem = document.createElement(tag);
+  newElem.innerHTML = text;
+  parent.appendChild(newElem);
+}
+
 function addSelect(optArray) {
+  //adds selection box for cities in mentors array
   //optArray - the things to add to the dropdown
   var newSelect = document.createElement('select');
+  var placeholder = document.createElement('option');
   var options = [];
+
+  placeholder.innerHTML = 'Choose a city.';
+  placeholder.setAttribute('selected', 'selected');
+  placeholder.setAttribute('disabled', 'disabled');
+  newSelect.appendChild(placeholder);
 
   newSelect.className = 'form-control';
   for (var i = 0; i < optArray.length; i++) {
